e053c52339bdd2580fe46bfb7b2c3da5161e5c0b
Ivan Yastrebov <easy.quest@ya.ru>
2023-01-05 22:44:49 +0300

    Last Sync: 2023.01.05 22:44:49

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # easy-zzz.github.io
 my first site
+
+[https://easy-zzz.github.io/](https://easy-zzz.github.io/)
---
+++ easy-zzz.github.io.sh
@@ -0,0 +1,16 @@
+#!/data/data/com.termux/files/usr/bin/env bash
+
+OBSIDIAN_PATH=/data/data/com.termux/files/home/Projects/repos/github.com/easy-zzz/easy-zzz.github.io
+cd $OBSIDIAN_PATH;
+
+CHANGES_EXIST=`git status --porcelain|wc -l`
+
+if [ "$CHANGES_EXIST" -eq 0 ]; then
+  exit 0
+fi
+
+# git pull
+git add --all
+git commit -q -m "Last Sync: $(date +"%Y.%m.%d %H:%M:%S")"
+git push #-q
+
